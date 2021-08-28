0fa5077b1a0ca04608aea09761e17832cf684879
Hanna Sabu Mampallil <hanblessab@gmail.com>
2021-08-29 01:12:09 +0530

    home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodindeed/main.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
@@ -11,15 +12,22 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Row(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.menu,
+                      size: 40,
+                    ),
+                  ),
                   Text("Food Indeed"),
                   Image.asset(
                     "assets/logo.png",
@@ -27,49 +35,168 @@ class _HomeState extends State<Home> {
                   ),
                 ],
               ),
-            ),
-            Text("Find food groups\naround you"),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: 150,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color.fromRGBO(25, 29, 49, 1)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Find food groups\naround you",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: 250,
                         child: Padding(
-                          padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                          padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
                           child: TextField(
+                            cursorColor: Colors.black,
                             style: GoogleFonts.poppins(
-                                color: Colors.white, fontSize: 16),
+                                color: Colors.black, fontSize: 16),
                             onChanged: (value) {
                               search = value;
                             },
                             decoration: InputDecoration(
-                              hintText: "search using city",
+                                border: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      color: Colors.black, width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      color: Colors.black, width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "search using city",
+                                hintStyle: TextStyle()),
+                          ),
+                        ),
+                      ),
+                      MaterialButton(
+                        minWidth: 60,
+                        height: 60,
+                        color: Color(0xFFEB271A),
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text(
+                              'Food Groups',
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                           ),
-                        )),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Container(
+                            width: 11,
+                            height: 190,
+                            color: Color(0xFFEB271A),
+                          ),
+                          // Container(
+                          //   width: 110,
+                          //   height: 5,
+                          //   color: Color(0xFFEB271A),
+                          // )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height / 4,
+                            width: MediaQuery.of(context).size.width - 50,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 5,
+                              itemBuilder: (BuildContext context, int index) =>
+                                  Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.pink,
+                                    borderRadius: BorderRadius.circular(10)),
+                                padding: EdgeInsets.all(10),
+                                margin: EdgeInsets.all(10),
+                                // height: 50,
+                                width: 250,
+                                // child: Text('1'),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Name',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Row(
+                              children: [
+                                Text('location'),
+                                SizedBox(
+                                  width: 30,
+                                ),
+                                Text('datetime')
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text('slots avaliable: '),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Icon(Icons.search),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Text("wanna start a new food group?"),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Text("Click\nHere"),
-                )
-              ],
-            )
-          ],
+                  Container(
+                    margin: EdgeInsets.only(left: 5),
+                    height: 7,
+                    width: MediaQuery.of(context).size.width - 50,
+                    color: Color(0xFFEB271A),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    "wanna start a new food \ngroup?",
+                    textAlign: TextAlign.right,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Material(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    color: Color(0xFFEB271A),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Click\nHere",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodindeed/home.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -5,7 +6,10 @@ import 'signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -41,6 +45,7 @@ class _LoginState extends State<Login> {
     return Scaffold(
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -54,39 +59,72 @@ class _LoginState extends State<Login> {
                   style: TextStyle(fontSize: 30),
                 ),
                 Text(
-                  "find food groups and communities around you",
+                  "find food groups and \ncommunities around you",
                   style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                   child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromRGBO(25, 29, 49, 1)),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
-                        child: TextField(
-                          style: GoogleFonts.poppins(
-                              color: Colors.white, fontSize: 16),
-                          onChanged: (value) {
-                            email = value;
-                          },
-                          decoration: InputDecoration(
-                            hintText: "email",
-                          ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(stops: [
+                        0.1,
+                        0.8,
+                        //0.98
+                      ], colors: [
+                        Color(0xFFEB271A),
+                        Color(0xFFFF7A00),
+                        //
+                        //Color.fromRGBO(196, 196, 196, 0),
+                      ]),
+                      color: Color.fromRGBO(25, 29, 49, 1),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                      child: TextField(
+                        cursorColor: Colors.white,
+                        cursorHeight: 24,
+                        style: GoogleFonts.poppins(
+                            color: Colors.white, fontSize: 16),
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                          disabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          hintStyle: TextStyle(color: Colors.white),
+                          hintText: "email",
                         ),
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                   child: Container(
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromRGBO(25, 29, 49, 1)),
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          stops: [
+                            0.1,
+                            0.8,
+                            //0.98
+                          ],
+                          colors: [
+                            Color(0xFFEB271A),
+                            Color(0xFFFF7A00),
+                            //
+                            //Color.fromRGBO(196, 196, 196, 0),
+                          ],
+                        ),
+                      ),
                       child: Padding(
                         padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
                         child: TextField(
@@ -97,51 +135,75 @@ class _LoginState extends State<Login> {
                           },
                           decoration: InputDecoration(
                             hintText: "password",
+                            disabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            hintStyle: TextStyle(color: Colors.white),
                           ),
                         ),
                       )),
                 ),
-                MaterialButton(
-                  onPressed: () {
-                    FirebaseAuth.instance
-                        .signInWithEmailAndPassword(
-                            email: email, password: password)
-                        .then((result) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                      );
-                    }).catchError((err) {
-                      print(err.message);
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text("Error"),
-                              content: Text(err.message),
-                              actions: [
-                                TextButton(
-                                  child: Text("Ok"),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                )
-                              ],
-                            );
-                          });
-                    });
-                  },
-                  child: Text("LogIn"),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFEB271A),
+                  ),
+                  child: MaterialButton(
+                    onPressed: () {
+                      FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: email, password: password)
+                          .then((result) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      }).catchError((err) {
+                        print(err.message);
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Error"),
+                                content: Text(err.message),
+                                actions: [
+                                  TextButton(
+                                    child: Text("Ok"),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  )
+                                ],
+                              );
+                            });
+                      });
+                    },
+                    child: Text(
+                      "LogIn",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontSize: 16),
+                    ),
+                  ),
                 ),
-                InkWell(
-                  child: Text("or signup here"),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Signup()));
-                  },
-                )
               ],
             ),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("or signup "),
+                  Text(
+                    'here',
+                    style: TextStyle(color: Color(0xFFEB271A)),
+                  )
+                ],
+              ),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              },
+            )
           ],
         ),
       ),
